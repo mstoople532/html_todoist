8277d898ed94adbd3d0f704d044c92a7bb4d821e
Russell Osborne <russell@burningpony.com>
2016-11-02 16:23:02 -0400

    Fix hosting of index.html, add a catchall rackup file

--- server/lib/app.rb
+++ server/lib/app.rb
@@ -1,28 +1,34 @@
 # This is used to select which database to use.
-ENV["RACK_ENV"] ||= 'development'
+ENV['RACK_ENV'] ||= 'development'
 
-require "bundler/setup"
-require "sinatra"
-require "json"
+require 'bundler/setup'
+require 'sinatra'
+require 'json'
 
 require_relative 'database'
 
 class App < Sinatra::Base
   # Serve any HTML/CSS/JS from the client folder
   set :static, true
-  set :public_folder, Proc.new { File.join(root, "..", "client", "public") }
+  set :public_folder, proc { File.join(root, '..', '..', 'client', 'public') }
 
   # Enable the session store
   enable :sessions
 
   # This ensures that we always return the content-type application/json
   before do
-    content_type "application/json"
+    content_type 'application/json'
   end
 
-  # You can delete this route
-  get "/api" do
-    {msg: 'The server is running'}.to_json
+  # DO NOT REMOVE THIS ENDPOINT IT ENABLES HOSTING HTML FOR THE FRONT END
+  get '/' do
+    content_type 'text/html'
+    body File.read(File.join(settings.public_folder, 'index.html'))
+  end
+
+  # You can delete this route but you should nest your endpoints under /api
+  get '/api' do
+    { msg: 'The server is running' }.to_json
   end
 
   # If this file is run directly boot the webserver
