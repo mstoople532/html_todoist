4917c82fb25a5215c9b593a468ae899f40a668f9
Russell Osborne <russell@theironyard.com>
2017-03-08 22:22:15 -0500

    Gut Client dir.

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import App from './components/app';
-import '../node_modules/semantic-ui-css/semantic.min.css';
-
-render(
-  <App />,
-  document.getElementById("root")
-);
