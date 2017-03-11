399136aca24a27627f67c58892f4d9d90fded906
Robby Dore <Rdore88@gmail.com>
2017-03-11 17:29:17 -0500

    Created more tests for the client side.

--- client/development.rb
+++ client/development.rb
@@ -5,4 +5,12 @@ class Development
   def get_tasks
     HTTParty.get("HTTP://localhost:4567/tasks")
   end
+
+  def complete_tasks(task_id)
+    HTTParty.patch("HTTP://localhost:4567/tasks/#{task_id}")
+  end
+
+  def add_task_to_list(list_id, task_id)
+    HTTParty.get("HTTP://localhost:4567/lists/#{list_id}/tasks/#{task_id}")
+  end
 end
