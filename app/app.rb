540195d6687c41ee6051407c78fd9db19b4c4540
Molly <mstoople@gmail.com>
2017-03-20 09:05:48 -0400

    ditto

--- app/app.rb
+++ app/app.rb
@@ -23,44 +23,41 @@ class App < Sinatra::Base
     erb :"new_task.html"
   end
 
-  get "/tasks/complete" do
-    completed_tasks = Task.all
-    to_be_response = completed_tasks.select do |el|
-      if el.completed_at
-        true
-      end
-    end
-    body to_be_response.to_json
-  end
+  # get "/tasks/complete" do
+  #   completed_tasks = Task.all
+  #   to_be_response = completed_tasks.select do |el|
+  #     if el.completed_at
+  #       true
+  #     end
+  #   end
+  #   body to_be_response.to_json
+  # end
 
-  get "/tasks/:id" do
-    body Task.find(params["id"]).to_json
-  end
+  # get "/tasks/:id" do
+  #   body Task.find(params["id"]).to_json
+  # end
 
   patch "/tasks/:id" do
-    payload = JSON.parse(request.body.read)
     task = Task.find(params["id"])
-    task.update(payload)
-    body task.to_json
+    task.update
   end
 
-  delete "/tasks/:id" do
-    task = Task.find(params["id"])
-    task.destroy
-    body task.to_json
-  end
+  # delete "/tasks/:id" do
+  #   @task = Task.find(params["id"])
+  #   @task.destroy
+  # end
 
   get "/lists" do
-    body List.all.to_json
+    @lists = List.all
   end
 
   post "/lists" do
-    payload = JSON.parse request.body.read
-    body List.create(payload).to_json
+    List.create(params)
+    redirect to("/lists")
   end
 
-  get "/lists/:id" do
-    body List.find(params["id"]).to_json
+  get "/lists/:name" do
+    List.where(params["name"])
   end
 
   get "/lists/:list_id/tasks/:task_id" do
@@ -73,18 +70,18 @@ class App < Sinatra::Base
     body Task.where(list_id: params["list_id"]).to_json
   end
 
-  patch "/lists/:id" do
-    payload = JSON.parse(request.body.read)
-    list = List.find(params["id"])
-    list.update(payload)
-    body list.to_json
-  end
+  # patch "/lists/:id" do
+  #   payload = JSON.parse(request.body.read)
+  #   list = List.find(params["id"])
+  #   list.update(payload)
+  #   body list.to_json
+  # end
 
-  delete "/lists/:id" do
-    list = List.find(params["id"])
-    list.destroy
-    body list.to_json
-  end
+  # delete "/lists/:id" do
+  #   list = List.find(params["id"])
+  #   list.destroy
+  #   body list.to_json
+  # end
 
   # If this file is run directly boot the webserver
   run! if app_file == $PROGRAM_NAME
--- db/migrations/20170317141552_create_lists_table_migration.rb
+++ db/migrations/20170317141552_create_lists_table_migration.rb
@@ -1,3 +1,5 @@
+require "active_record"
+
 class CreateListsTableMigration < ActiveRecord::Migration[5.0]
   def change
     create_table :lists do |t|
